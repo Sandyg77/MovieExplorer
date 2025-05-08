bae159d708ed16641f2de2acb13cd6c5a81f9bda
Sandyg77 <163460477+Sandyg77@users.noreply.github.com>
2025-05-09 02:25:57 +0530

    Updated MovieCard

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,45 @@
+// src/components/MovieCard.jsx
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Box,
+  Rating,
+} from "@mui/material";
+
+const MovieCard = ({ movie, onClick }) => {
+  return (
+    <Card
+      onClick={() => onClick(movie)}
+      sx={{ cursor: "pointer", height: "100%" }}
+    >
+      <CardMedia
+        component="img"
+        image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+        sx={{ height: 300 }}
+      />
+      <CardContent>
+        <Typography variant="h6" gutterBottom noWrap>
+          {movie.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {new Date(movie.release_date).getFullYear()}
+        </Typography>
+        <Box mt={1}>
+          <Rating
+            name="read-only"
+            value={movie.vote_average / 2}
+            precision={0.5}
+            readOnly
+            size="small"
+          />
+        </Box>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MovieCard;
